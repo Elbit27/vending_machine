be22abcabd82050fca38b00ee2d1cebbd0aa6d86
elbur27 <mamatzhanov1397@gmail.com>
2025-02-27 15:03:05 +0600

    pagination for product list

--- product/views.py
+++ product/views.py
@@ -2,9 +2,21 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Product
 from . import serializers
 
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class StandardResultPagination(PageNumberPagination):
+    page_size = 3
+    page_query_param = 'page'
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
+    pagination_class = StandardResultPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ('id', 'name')
 
     def perform_create(self, serializer):
         serializer.save()
